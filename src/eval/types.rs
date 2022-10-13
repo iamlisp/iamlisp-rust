e0051daeb5c0014d33d91a00c2e0446047351ee0
roman <roman.lakhtadyr@gmail.com>
2022-10-13 14:55:33 +0100

    feat: list iter + tests

--- src/eval/types.rs
+++ src/eval/types.rs
@@ -1,29 +1,90 @@
+use std::collections::HashMap;
+
 #[derive(Clone)]
-pub(crate) struct List {
-    car: Expression,
-    cdr: Option<List>,
+pub(crate) enum List {
+    Empty,
+    NonEmpty { car: Expression, cdr: Box<List> },
 }
 
+pub(crate) const EMPTY_LIST: List = List::Empty;
+
 impl List {
-    pub(crate) fn new(car: Expression, cdr: Option<List>) -> Self {
-        List { car, cdr }
+    pub(crate) fn new(car: Expression, cdr: List) -> Self {
+        List::NonEmpty {
+            car,
+            cdr: Box::new(cdr),
+        }
     }
 
-    pub(crate) fn car(&self) -> &Expression {
-        &self.car
+    pub(crate) fn car(&self) -> Option<&Expression> {
+        match self {
+            List::Empty => None,
+            List::NonEmpty { car, cdr: _ } => Some(car),
+        }
     }
 
-    pub(crate) fn cdr(&self) -> &Option<List> {
-        &self.cdr
+    pub(crate) fn cdr(&self) -> &List {
+        match self {
+            List::Empty => &EMPTY_LIST,
+            List::NonEmpty { car: _, cdr } => cdr,
+        }
+    }
+
+    pub(crate) fn is_empty(&self) -> bool {
+        matches!(self, List::Empty)
+    }
+}
+
+impl Iterator for List {
+    type Item = Expression;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let current = self.car().cloned();
+
+        *self = self.cdr().clone();
+
+        current
     }
 }
 
 #[derive(Clone)]
 pub(crate) enum Expression {
     Int(i64),
-    List(List),
+    List(Box<List>),
     Nil,
 }
 
 #[derive(Clone)]
-pub(crate) struct Env {}
+pub(crate) struct Env {
+    values: HashMap<&'static str, Expression>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty_iter() {
+        let vec = List::Empty.into_iter().collect::<Vec<_>>();
+
+        assert_eq!(vec.len(), 0);
+    }
+
+    #[test]
+    fn one_item_iter() {
+        let vec = List::new(Expression::Nil, List::Empty)
+            .into_iter()
+            .collect::<Vec<_>>();
+
+        assert_eq!(vec.len(), 1);
+    }
+
+    #[test]
+    fn two_items_iter() {
+        let vec = List::new(Expression::Nil, List::new(Expression::Nil, List::Empty))
+            .into_iter()
+            .collect::<Vec<_>>();
+
+        assert_eq!(vec.len(), 2);
+    }
+}
