3ab9be35cf9f123974875a93b250181712cec5c1
Roman L <roman.lakhtadyr@gmail.com>
2022-10-27 18:44:07 +0100

    feat: add more tests to compiler and create CHANGELOG.md

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# 0.1.0-alpha.1
+
+* **read**: Implemented program compiler.
+
+Note: No tracked changes prior to this release.
--- src/read/mod.rs
+++ src/read/mod.rs
@@ -21,6 +21,60 @@ mod tests {
     use crate::{list, symbol};
 
     #[test]
+    fn read_nested_lists() {
+        let program = r#"(() ()) ()"#;
+
+        assert_eq!(
+            list![list![list![].into(), list![].into()].into(), list![].into()],
+            compile(program).unwrap()
+        );
+    }
+
+    #[test]
+    fn read_boolean() {
+        assert_eq!(list![Value::Bool(true).into()], compile("true").unwrap());
+        assert_eq!(list![Value::Bool(false).into()], compile("false").unwrap());
+    }
+
+    #[test]
+    fn read_int() {
+        assert_eq!(list![Value::Int64(0).into()], compile("0").unwrap());
+        assert_eq!(list![Value::Int64(10).into()], compile("10").unwrap());
+        assert_eq!(list![Value::Int64(-10).into()], compile("-10").unwrap());
+    }
+
+    #[test]
+    fn read_float() {
+        assert_eq!(list![Value::Float64(0.0).into()], compile("0.0").unwrap());
+        assert_eq!(list![Value::Float64(10.0).into()], compile("10.0").unwrap());
+        assert_eq!(
+            list![Value::Float64(-10.0).into()],
+            compile("-10.0").unwrap()
+        );
+    }
+
+    #[test]
+    fn read_string() {
+        assert_eq!(
+            list![Value::String("".to_string()).into()],
+            compile(r#""""#).unwrap()
+        );
+
+        assert_eq!(
+            list![Value::String("hello world".to_string()).into()],
+            compile(r#""hello world""#).unwrap()
+        );
+    }
+
+    #[test]
+    fn read_symbol() {
+        assert_eq!(
+            list![symbol!("foo"), symbol!("bar")],
+            compile("foo bar").unwrap()
+        );
+    }
+
+    #[test]
     fn read_whole_program() {
         let program = r#"(+ (foo 1 "hello") 12.5)"#;
 
