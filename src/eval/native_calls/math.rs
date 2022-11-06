c704181bb8216d52e0f8ba87b883324fee8c7c9a
Roman L <roman.lakhtadyr@gmail.com>
2022-11-06 14:15:24 +0000

    refactor: reduce clone

--- src/eval/native_calls/math.rs
+++ src/eval/native_calls/math.rs
@@ -14,26 +14,22 @@ impl Op for Multiply {
 
     fn apply(&self, args: &List<Expression>, _env: &Env) -> anyhow::Result<Expression> {
         Ok(match args.head() {
-            Some(Expression::Value(Value::Int64(_))) => Value::Int64(
-                List::clone(&args)
-                    .into_iter()
-                    .try_fold(1, |acc, exp| match exp {
-                        Expression::Value(Value::Int64(val)) => Ok(acc * val),
-                        Expression::Value(Value::Float64(val)) => Ok(acc * (val as i64)),
-                        x => bail!("Unable to multiply {} by {}", acc, x),
-                    })?,
-            )
-            .into(),
-            Some(Expression::Value(Value::Float64(_))) => Value::Float64(
-                List::clone(&args)
-                    .into_iter()
-                    .try_fold(1.0, |acc, exp| match exp {
-                        Expression::Value(Value::Int64(val)) => Ok(acc * (val as f64)),
-                        Expression::Value(Value::Float64(val)) => Ok(acc * val),
-                        x => bail!("Unable to multiply {} by {}", acc, x),
-                    })?,
-            )
-            .into(),
+            Some(Expression::Value(Value::Int64(_))) => {
+                Value::Int64(args.iter().try_fold(1, |acc, exp| match exp {
+                    Expression::Value(Value::Int64(val)) => Ok(acc * *val),
+                    Expression::Value(Value::Float64(val)) => Ok(acc * (*val as i64)),
+                    x => bail!("Unable to multiply {} by {}", acc, x),
+                })?)
+                .into()
+            }
+            Some(Expression::Value(Value::Float64(_))) => {
+                Value::Float64(args.iter().try_fold(1.0, |acc, exp| match exp {
+                    Expression::Value(Value::Int64(val)) => Ok(acc * (*val as f64)),
+                    Expression::Value(Value::Float64(val)) => Ok(acc * *val),
+                    x => bail!("Unable to multiply {} by {}", acc, x),
+                })?)
+                .into()
+            }
             _ => bail!(
                 "Function not implemented for this kind of arguments: {}",
                 args
@@ -83,7 +79,7 @@ pub(crate) struct Pow;
 
 impl Op for Pow {
     fn name(&self) -> &'static str {
-        "%"
+        "pow"
     }
 
     fn apply(&self, args: &List<Expression>, _env: &Env) -> anyhow::Result<Expression> {
@@ -122,26 +118,22 @@ impl Op for Subtract {
             Some(Expression::Value(Value::Float64(init))) if args.tail().is_empty() => {
                 Value::Float64(-*init).into()
             }
-            Some(Expression::Value(Value::Int64(init))) => Value::Int64(
-                List::clone(args.tail())
-                    .into_iter()
-                    .try_fold(*init, |acc, exp| match exp {
-                        Expression::Value(Value::Int64(val)) => Ok(acc - val),
-                        Expression::Value(Value::Float64(val)) => Ok(acc - (val as i64)),
-                        x => bail!("Unable to subtract {} and {}", acc, x),
-                    })?,
-            )
-            .into(),
-            Some(Expression::Value(Value::Float64(init))) => Value::Float64(
-                List::clone(args.tail())
-                    .into_iter()
-                    .try_fold(*init, |acc, exp| match exp {
-                        Expression::Value(Value::Int64(val)) => Ok(acc - (val as f64)),
-                        Expression::Value(Value::Float64(val)) => Ok(acc - val),
-                        x => bail!("Unable to subtract {} and {}", acc, x),
-                    })?,
-            )
-            .into(),
+            Some(Expression::Value(Value::Int64(init))) => {
+                Value::Int64(args.tail().iter().try_fold(*init, |acc, exp| match exp {
+                    Expression::Value(Value::Int64(val)) => Ok(acc - *val),
+                    Expression::Value(Value::Float64(val)) => Ok(acc - (*val as i64)),
+                    x => bail!("Unable to subtract {} and {}", acc, x),
+                })?)
+                .into()
+            }
+            Some(Expression::Value(Value::Float64(init))) => {
+                Value::Float64(args.tail().iter().try_fold(*init, |acc, exp| match exp {
+                    Expression::Value(Value::Int64(val)) => Ok(acc - (*val as f64)),
+                    Expression::Value(Value::Float64(val)) => Ok(acc - *val),
+                    x => bail!("Unable to subtract {} and {}", acc, x),
+                })?)
+                .into()
+            }
             _ => bail!(
                 "Function not implemented for this kind of arguments: {}",
                 args
@@ -160,26 +152,22 @@ impl Op for Sum {
 
     fn apply(&self, args: &List<Expression>, _env: &Env) -> anyhow::Result<Expression> {
         Ok(match args.head() {
-            Some(Expression::Value(Value::Int64(_))) => Value::Int64(
-                List::clone(&args)
-                    .into_iter()
-                    .try_fold(0, |acc, exp| match exp {
-                        Expression::Value(Value::Int64(val)) => Ok(acc + val),
-                        Expression::Value(Value::Float64(val)) => Ok(acc + (val as i64)),
-                        x => bail!("Unable to sum {} and {}", acc, x),
-                    })?,
-            )
-            .into(),
-            Some(Expression::Value(Value::Float64(_))) => Value::Float64(
-                List::clone(&args)
-                    .into_iter()
-                    .try_fold(0.0, |acc, exp| match exp {
-                        Expression::Value(Value::Int64(val)) => Ok(acc + (val as f64)),
-                        Expression::Value(Value::Float64(val)) => Ok(acc + val),
-                        x => bail!("Unable to sum {} and {}", acc, x),
-                    })?,
-            )
-            .into(),
+            Some(Expression::Value(Value::Int64(_))) => {
+                Value::Int64(args.iter().try_fold(0, |acc, exp| match exp {
+                    Expression::Value(Value::Int64(val)) => Ok(acc + *val),
+                    Expression::Value(Value::Float64(val)) => Ok(acc + (*val as i64)),
+                    x => bail!("Unable to sum {} and {}", acc, x),
+                })?)
+                .into()
+            }
+            Some(Expression::Value(Value::Float64(_))) => {
+                Value::Float64(args.iter().try_fold(0.0, |acc, exp| match exp {
+                    Expression::Value(Value::Int64(val)) => Ok(acc + (*val as f64)),
+                    Expression::Value(Value::Float64(val)) => Ok(acc + *val),
+                    x => bail!("Unable to sum {} and {}", acc, x),
+                })?)
+                .into()
+            }
             _ => bail!(
                 "Function not implemented for this kind of arguments: {}",
                 args
